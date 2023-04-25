f4c6068081a53f2dc8a7cad52608863b7503a455
Rékási László <rekalaca@gmail.com>
2023-04-25 22:43:52 +0200

    mail

--- mail.php
+++ mail.php
@@ -17,96 +17,85 @@ echo'
 <html lang="hu">
 
 <head>
-
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>rekalaca webdesign | reszponzív, mobilbarát weboldalak készítése!</title>
-  <meta name="description"
-    content="Reszponzív, mobilbarát Weboldalak tervezése és kivitelezése kedvező áron!/Nyíregyháza">
-  <meta name="robots" content="index, folow">
-  <link rel="canonical" href="https://www.rekalaca-webdesign.hu/">
-  <meta name="keywords"
-    content="reszponzív, weboldalkészítés, webdesign, weblapkészítés, honlapkészítés, weblap karbantartás, Nyíregyháza,rekalaca webdesign">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-
-  <!-- Google Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-    crossorigin="anonymous">
-    </script>
-  <script>
-    $(function () {
-      $("#header").load("header.html");
-      $("#footer").load("footer.html");
-    });
-  </script>
-
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Legyen SAJÁT weboldala!</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap Icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+    <!-- SimpleLightbox plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
 </head>
 
-<body>
-  
-
-  <!-- ======= Hero Section ======= -->
-  <section id="hero">
-    <div class="hero-container">
-      <a href="index.html" class="hero-logo" data-aos="zoom-in"><img src="assets/img/newlogo/Rekalaca.png" alt=""
-          style="max-width: 300px;"></a>
-      <h1 data-aos="zoom-in">Köszönöm megkeresését!</h1>
-      <h2 data-aos="fade-up">Hamarosan felveszem Önnel a kapcsolatot!</h2>
-      <a data-aos="fade-up" href="index.html" class="btn-get-started scrollto">Vissza a Főoldalra!</a>
-    </div>
-  </section>
-  <!-- End Hero -->
-
-
-
-
-  <main id="main">
-
-
-  <!-- ======= Footer Section ======= -->
-  <div id="footer"></div>
-  <!-- ======= End Footer Section ======= -->
-
-</main><!-- End #main -->
-
-
-
-
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center upbutton"><i
-    class="bi bi-arrow-up-short"></i></a>
-
-
-
-<!-- Vendor JS Files -->
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-
-
-
-
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#page-top">Kezdőlap</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#about">Rólunk</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#services">Szolgáltatások</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">Galéria</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#price">Árak</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Kapcsolat</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="container px-4 px-lg-5 h-100">
+            <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                <div class="col-lg-12 align-self-end">
+                    <h1 class="text-white font-weight-bold">Köszönjük megkeresését!</h1>
+                    <hr class="divider" />
+                </div>
+                <div class="col-lg-12 align-self-baseline">
+                    <h5 class="magyaraz">Hamarosan felvesszük Önnel a kapcsolatot!</h5>
+                    <br>
+                    
+                    <br>
+                    <a class="btn btn-primary btn-xl" href="index.html">Vissza a főoldalra!</a>
+                </div>
+            </div>
+        </div>
+    </header>
+   
+   
+    <!-- Footer-->
+    <footer class="bg-dark py-5">
+        <div class="container px-4 px-lg-5">
+            <div class="small text-center copys">Copyright &copy; 2023<img class="img-fluid logom" src="assets/logo-no-background.png" alt="..."></div>
+            
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- SimpleLightbox plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
